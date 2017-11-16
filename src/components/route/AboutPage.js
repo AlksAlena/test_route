8a66095c35411ed53d26d9182c651f73920629a7
Helen <alksalena@gmail.com>
2017-11-16 11:24:17 +0600

    Init src/components/route/AboutPage.js

---
+++ src/components/route/AboutPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './AboutPage.less';
+
+export default class AboutPage extends Component {
+  render() {
+    return(
+      <div className="AboutPage">
+        <h2 className="title">
+          This is messages application
+        </h2>
+
+        <div className="text">
+          <p>Lorem djkhlo vsvlsa dhjgfdf sjhgufhdl fejgd kdhfjk jkdegfl lffgd dgddf</p>
+          <p>Lorem djkhlo vsvlsa dhjgfdf sjhgufhdl fejgd kdhfjk jkdegfl lffgd dgddf</p>
+          <p>Lorem djkhlo vsvlsa dhjgfdf sjhgufhdl fejgd kdhfjk jkdegfl lffgd dgddf</p>
+          <p>Lorem djkhlo vsvlsa dhjgfdf sjhgufhdl fejgd kdhfjk jkdegfl lffgd dgddf</p>
+          <p>Lorem djkhlo vsvlsa dhjgfdf sjhgufhdl fejgd kdhfjk jkdegfl lffgd dgddf</p>
+        </div>
+      </div>
+    );
+  }
+}
